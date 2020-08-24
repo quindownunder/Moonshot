e5ec7f27dd9c89672329b46525d559b2e5372321
Matthew Richardson <mat_richardson@mac.com>
2020-08-25 09:12:37 +1000

    Tutorial app complete

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Matthew Richardson on 24/8/20.
+//  Copyright © 2020 Matthew Richardson. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
