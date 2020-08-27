e8432b80bc123b6872d702b391b1c8cebc54afd2
Matthew Richardson <mat_richardson@mac.com>
2020-08-27 15:15:35 +1000

    Completed exercise 3

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,15 +8,47 @@
 
 import SwiftUI
 
+struct CrewList: View {
+    
+    var astronauts = [CrewMember]()
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(self.astronauts, id: \.role) { crewMember in
+                Text(crewMember.astronaut.name)
+            }
+        }
+    }
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        
+        var matches = [CrewMember]()
+
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+        
+        self.astronauts = matches
+    }
+}
+
 struct ContentView: View {
     
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    @State private var showingCrew = false
     
+    @State var crewNames = [String]()
+        
     var body: some View {
         NavigationView {
             List(missions) { mission in
                 NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts, missions: self.missions)) {
+                    
                     Image(mission.image)
                         .resizable()
                         .scaledToFit()
@@ -25,11 +57,24 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         Text(mission.displayName)
                             .font(.headline)
-                        Text(mission.formattedLaunchDate)
+                        if self.showingCrew {
+                            CrewList(mission: mission, astronauts: self.astronauts)
+                            
+                        } else {
+                            Text(mission.formattedLaunchDate)
+                        }
                     }
+                    
                 }
             }
             .navigationBarTitle("Moonshot")
+            .navigationBarItems(leading:
+                Button(action: {
+                    self.showingCrew.toggle()
+                }) {
+                    Image(systemName: self.showingCrew ? "calendar" : "person.3")
+                }
+            )
         }
     }
 }
